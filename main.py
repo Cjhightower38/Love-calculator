f7a61bb0d9e415a126b53c77dd86c3a35f4e6fdd
Cornelius Jermaine Hightower <75323116+Cjhightower38@users.noreply.github.com>
2022-01-22 00:45:57 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,38 @@
+# 🚨 Don't change the code below 👇
+print("Welcome to the Love Calculator!")
+name1 = input("What is your name? \n")
+name2 = input("What is their name? \n")
+# 🚨 Don't change the code above 👆
+
+#Write your code below this line 👇
+
+new_name1=name1.casefold()
+new_name2=name2.casefold()
+new_name3 = new_name1 +' '+ new_name2
+
+t = new_name3.count('t')
+r = new_name3.count('r')
+u = new_name3.count('u')
+e = new_name3.count('e')
+
+l = new_name3.count('l')
+o = new_name3.count('o')
+v = new_name3.count('v')
+e = new_name3.count('e')
+
+true = t + r + u + e
+love = l + o + v + e
+true_love = str(true) + str(love)
+
+# print(true_love)
+
+true_love_score = int(true_love)
+
+if (true_love_score < 10) or (true_love_score > 90):
+    print(f'Your score is {true_love_score}, you go together like coke and mentos.')
+elif (true_love_score >= 40) and (true_love_score <= 50):
+    print(f'Your score is {true_love_score}, you are alright together.')
+else:
+    print(f'Your score is {true_love_score}.')
+
+
