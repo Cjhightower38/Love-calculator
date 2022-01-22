17f5f3656ca82236d0a876f5d065f93a36720a10
Cornelius Jermaine Hightower <75323116+Cjhightower38@users.noreply.github.com>
2022-01-22 00:53:31 +0000

    Add documentation for .casefold() or .lower().

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ name2 = input("What is their name? \n")
 # 🚨 Don't change the code above 👆
 
 #Write your code below this line 👇
-
+# .lower() could also be use but .casefold() will make even Bobby JAMes lower case .lower() wouls not.
 new_name1=name1.casefold()
 new_name2=name2.casefold()
 new_name3 = new_name1 +' '+ new_name2
@@ -25,12 +25,13 @@ love = l + o + v + e
 true_love = str(true) + str(love)
 
 # print(true_love)
+# true_love_score = int(true_love) or true_love = int(str(true) + str(love)) could be used to fix the concatenation error.
 
 true_love_score = int(true_love)
 
-if (true_love_score < 10) or (true_love_score > 90):
+if true_love_score < 10 or true_love_score > 90:
     print(f'Your score is {true_love_score}, you go together like coke and mentos.')
-elif (true_love_score >= 40) and (true_love_score <= 50):
+elif true_love_score >= 40 and true_love_score <= 50:
     print(f'Your score is {true_love_score}, you are alright together.')
 else:
     print(f'Your score is {true_love_score}.')
